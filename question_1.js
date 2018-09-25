10ce893067d738209f42a972b13b12e936743e58
Gaayathre <42710981+Gaayathre@users.noreply.github.com>
2018-09-25 00:06:26 -0700

    Add files via upload

---
+++ question_1.js
@@ -0,0 +1,9 @@
+//code to print name , age , date_of_birth , and address
+let name='Vinny';
+let age=20;
+let dob='18-05-1999';
+let address='Eluru west Godavari Dist AndhraPradesh'
+console.log('My name is    '+name);
+console.log('I am   '+age+'   years old');
+console.log('date of birth    '+dob);
+console.log('adress:   '+address);
